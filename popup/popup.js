eabe06cd76dc847546439be573ba2708d8d7c0c8
saperkz <64010062+saperkz@users.noreply.github.com>
2023-08-14 19:11:18 +0500

    Add files via upload

---
+++ popup/popup.js
@@ -0,0 +1,6 @@
+var inputTextValue = document.getElementById('itsmNum')
+
+document.getElementById("NewTab").addEventListener("click", myFunction);
+    function myFunction() {
+            window.open('https://www.blalbalksfffff.com'+encodeURIComponent(inputTextValue.value), "_blank");
+        }
