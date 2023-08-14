baf8a3e8807b59596afdc79306fb04663ac12af4
saperkz <64010062+saperkz@users.noreply.github.com>
2023-08-15 00:07:02 +0600

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,16 @@
 var inputTextValue = document.getElementById('itsmNum')
 
-document.getElementById("NewTab").addEventListener("click", myFunction);
-    function myFunction() {
-            window.open('https://www.blalbalksfffff.com'+encodeURIComponent(inputTextValue.value), "_blank");
+document.getElementById("NewTab").addEventListener("click", OpenURL);
+function OpenURL() {
+	if  (inputTextValue.value.toString().startsWith("9")==true) {
+            window.open('https://iffffffnine.com'+encodeURIComponent(inputTextValue.value)+'999999', "_blank");
+	}
+	else {
+	window.alert(inputTextValue.value.toString());	
+  //window.open('https://www.elseeeeeeeee.com'+encodeURIComponent(inputTextValue.value)+'77777777', "_blank");
+}
+console.log(inputTextValue)
         }
+		
+		
+//document.write(5 + 6);
