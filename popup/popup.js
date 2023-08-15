e9564f058c0829d83e0d939b2949163e77d9c008
saperkz <64010062+saperkz@users.noreply.github.com>
2023-08-15 15:56:04 +0500

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,27 @@
 var inputTextValue = document.getElementById('itsmNum')
-
+var base_url = 'https://hscskp2.kio.kz:443/sap/bc/ags_workcenter/ags_crm_docln?objectid='
 document.getElementById("NewTab").addEventListener("click", OpenURL);
+inputTextValue.addEventListener("keypress", function OnEvent(event) {
+	if (event.key == "Enter"){OpenURL()}
+});
+
+
 function OpenURL() {
-	if  (inputTextValue.value.toString().startsWith("9")==true) {
-            window.open('https://iffffffnine.com'+encodeURIComponent(inputTextValue.value)+'999999', "_blank");
+	if  (inputTextValue.value.toString().trim().startsWith("8")==true) {
+            window.open(base_url+encodeURIComponent(inputTextValue.value.toString().trim())+'&proctype=ZMIN&', "_blank");
+	}
+	else if  (inputTextValue.value.toString().trim().startsWith("7")==true) {
+            window.open(base_url+encodeURIComponent(inputTextValue.value.toString().trim())+'&proctype=ZMHF&', "_blank");
+	}
+	else if  (inputTextValue.value.toString().trim().startsWith("9")==true) {
+            window.open(base_url+encodeURIComponent(inputTextValue.value.toString().trim())+'&proctype=YMRQ&', "_blank");
 	}
 	else {
-	window.alert(inputTextValue.value.toString());	
-  //window.open('https://www.elseeeeeeeee.com'+encodeURIComponent(inputTextValue.value)+'77777777', "_blank");
-}
-console.log(inputTextValue)
-        }
+	window.alert("Wrong number!");	
+    //window.alert(inputTextValue.value.toString());	
+ }
+
+ }
 		
 		
 //document.write(5 + 6);
